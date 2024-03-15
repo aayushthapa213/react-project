1afd51dbe647bf3ae0a774e99056dadc77505982
Aayush Thapa <aayushthapaa213@gmail.com>
2024-03-15 16:22:06 +0545

    version 2

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App (){
         Hello  World! This is an alert message. 
       </Alert>}
       <Button color="primary" onClick={()=>{setalertVisibility(true)}}>
-        button
+        BUTTON
       </Button>
     </div>
   )
